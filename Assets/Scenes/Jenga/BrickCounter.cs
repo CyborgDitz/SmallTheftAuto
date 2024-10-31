5f2489e15f18f22c9943da7b3c9841cb0c4cca20
CyborgDitz <sebban.lol@gmail.com>
2024-10-31 14:43:43 +0100

    Copied code to BrickCOunter

--- Assets/Scenes/Jenga/BrickCounter.cs
+++ Assets/Scenes/Jenga/BrickCounter.cs
@@ -4,15 +4,10 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private int brickCount;
+   public void CountBrick()
+   {
+       brickCount++;
+       Debug.Log($"Total Bricks Removed: {brickCount}");
+   }
 }
